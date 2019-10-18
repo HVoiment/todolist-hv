f06daea7871193f7013dbf0229e1ec0f4d9925fd
Henri Voiment <henri.voiment@capgemini.com>
2019-10-18 19:15:56 +0200

    Création de la methode getListsForUser dans ListManager
    implementation des tests relatifs à cette méthode

--- hv-todolist-business/src/main/java/hv/todolist/business/managers/ListManager.java
+++ hv-todolist-business/src/main/java/hv/todolist/business/managers/ListManager.java
@@ -1,5 +1,22 @@
 package hv.todolist.business.managers;
 
+import java.util.List;
+
+import hv.todolist.consumer.impl.hibernate.ListsDAOImpl;
+import hv.todolist.model.beans.ListBean;
+import hv.todolist.model.beans.UserBean;
+
 public class ListManager {
+
+	
+	/**
+	 * Retourne la liste de listes de l'utilisateur
+	 * @param user Bean de l'utilisateur choisi
+	 * @return
+	 */
+	public List<ListBean> getListsFor(UserBean user) {
+		ListsDAOImpl listsDAOImpl = new ListsDAOImpl();
+		return listsDAOImpl.listLists();
+	}
 
 }
